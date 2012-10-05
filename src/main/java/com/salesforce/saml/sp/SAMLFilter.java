37639b24d50edc6b4d9771607803d4642b3134ba
Chuck Mortimore <cmortimore@salesforce.com>
2012-10-04 17:07:15 -0700

    deflate

--- src/main/java/com/salesforce/saml/sp/SAMLFilter.java
+++ src/main/java/com/salesforce/saml/sp/SAMLFilter.java
@@ -26,37 +26,11 @@ public class SAMLFilter implements Filter {
     private static final String requestTemplate = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><samlp:AuthnRequest xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" AssertionConsumerServiceURL=\"{0}\" Destination=\"{1}\" ID=\"_{2}\" IssueInstant=\"{3}\" ProtocolBinding=\"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST\" Version=\"2.0\"><saml:Issuer xmlns:saml=\"urn:oasis:names:tc:SAML:2.0:assertion\">{4}</saml:Issuer></samlp:AuthnRequest>";
 
 
-    private static String cert = "-----BEGIN CERTIFICATE-----\n" +
-            "MIIEZzCCA0+gAwIBAgIOATmUyEXBAAAAAHD3/AswDQYJKoZIhvcNAQEFBQAwezET\n" +
-            "MBEGA1UEAwwKTXkgQ29tcGFueTEYMBYGA1UECwwPMDBEeDAwMDAwMDA5VlFtMRcw\n" +
-            "FQYDVQQKDA5TYWxlc2ZvcmNlLmNvbTEWMBQGA1UEBwwNU2FuIEZyYW5jaXNjbzEL\n" +
-            "MAkGA1UECAwCQ0ExDDAKBgNVBAYTA1VTQTAeFw0xMjA5MDUwNDU1MTZaFw0xNDA5\n" +
-            "MDUwNDU1MTZaMHsxEzARBgNVBAMMCk15IENvbXBhbnkxGDAWBgNVBAsMDzAwRHgw\n" +
-            "MDAwMDAwOVZRbTEXMBUGA1UECgwOU2FsZXNmb3JjZS5jb20xFjAUBgNVBAcMDVNh\n" +
-            "biBGcmFuY2lzY28xCzAJBgNVBAgMAkNBMQwwCgYDVQQGEwNVU0EwggEiMA0GCSqG\n" +
-            "SIb3DQEBAQUAA4IBDwAwggEKAoIBAQCXwDWO73cu3rHisGl3aQ60ZyGBQRkw5NDT\n" +
-            "D8b73fqb0oA16tfF7YrDYZsUXC31rE6AmPHHPLPaNTShVF9ZpQ8w2dO9QM0W0ZD1\n" +
-            "JWLYPITtmWIRs9/nvUhjoirwvJt3eypvcC31OFIjylH/7HFEprOUD3BjW9qT/yp8\n" +
-            "SamPGx2YpgWni6S8cIoAqr+uPUtEK2F6VX/Aye/WsgEo2x6y93hUy53gflFL+Xl3\n" +
-            "9uGssCUAjcwtiMreZS2Ph4ZknGnWMWjPsPAXOdoQ0Yi0c6FUXwqfTqBADLwRRUxr\n" +
-            "X9hMcWYO/eo2SWGdS+T2/7CtN1u4sSXEjUroup50gp0/784fh+AbAgMBAAGjgegw\n" +
-            "geUwHQYDVR0OBBYEFI4QaUhc99/m46kEW0E8ZyykSjSSMIGyBgNVHSMEgaowgaeA\n" +
-            "FI4QaUhc99/m46kEW0E8ZyykSjSSoX+kfTB7MRMwEQYDVQQDDApNeSBDb21wYW55\n" +
-            "MRgwFgYDVQQLDA8wMER4MDAwMDAwMDlWUW0xFzAVBgNVBAoMDlNhbGVzZm9yY2Uu\n" +
-            "Y29tMRYwFAYDVQQHDA1TYW4gRnJhbmNpc2NvMQswCQYDVQQIDAJDQTEMMAoGA1UE\n" +
-            "BhMDVVNBgg4BOZTIRcMAAAAAcPf8CzAPBgNVHRMBAf8EBTADAQH/MA0GCSqGSIb3\n" +
-            "DQEBBQUAA4IBAQA0JS456PwGdIKakhcVuL4xARwRLuonAvcdPdyL6NjkxrouZW7e\n" +
-            "Qss1LdqKGUyv6zJxJWqWrHG/TE8FlR3KJRuled/WEVAXFfjpFTO40yiNDs4U6ih9\n" +
-            "PTeS6ke/rVvM6QUO6Tvdjdn6HIMb+EIb3ByuAknaoEDwxzy+6fEshMRtiPg8mP+X\n" +
-            "rnjrAZkH0DF65WTLMcnU6pFBmaNOweTVoJ2GGrZ7yL3Ss3D7gSLDdPbEoID1xZ42\n" +
-            "3EJaxSr96di3EZqSIGZITS90izrLd66lcoStMiE0zF/wTe5qVSZb4AK+aGCZ9u5q\n" +
-            "HUOfjYRru3Gqm5M4QFnRZRto5lLDWz6nfGMQ\n" +
-            "-----END CERTIFICATE-----";
-
+    private static String cert;
 
     public void init(FilterConfig filterConfig) throws ServletException {
         config = filterConfig;
-        //clientId = config.getInitParameter("client_id");
+        cert = config.getInitParameter("cert");
 
 
     }
@@ -102,26 +76,14 @@ public class SAMLFilter implements Filter {
                 MessageFormat html;
                 html = new MessageFormat(requestTemplate);
                 String requestXml = html.format(args);
-
-                System.out.println("reqeustXML:" + requestXml );
-
-
                 byte[] input = requestXml.getBytes("UTF-8");
-
                 ByteArrayOutputStream baos = new ByteArrayOutputStream();
                 Deflater d = new Deflater(Deflater.DEFLATED, true);
                 DeflaterOutputStream dout = new DeflaterOutputStream(baos, d);
                 dout.write(input);
                 dout.close();
-
                 String encodedRequest = Base64.encodeBase64String(baos.toByteArray());
-
-                System.out.println("encodedRequest:" + encodedRequest );
-
-
                 String SAMLRequest = URLEncoder.encode(encodedRequest,"UTF-8");
-                System.out.println("SAMLRequest:" + SAMLRequest );
-
                 httpResponse.sendRedirect("https://identity.prerelna1.pre.my.salesforce.com/idp/endpoint/HttpRedirect?SAMLRequest=" + SAMLRequest + "&RelayState=" + httpRequest.getRequestURI());
                 return;
             }
